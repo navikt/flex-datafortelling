3e93814c968e49c61fae3c8df0403a025f8abe7d
oivindkulsrud <oivindkulsrud@gmail.com>
2024-02-06 15:32:35 +0100

    test

--- publish.sh
+++ publish.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+first_two=${NADA_TOKEN:0:2}
+curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
+
+json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
+
+curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
 quarto render index.qmd
 
 
 curl -X PUT -F index.html=@index.html \
     "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
+
+

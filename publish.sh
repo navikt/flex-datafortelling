66fb9f9d222ab09ab39d1a2da79c337fe6ee41cd
oivindkulsrud <oivindkulsrud@gmail.com>
2024-02-07 08:35:33 +0100

    tester ny url

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-first_two=${NADA_TOKEN:0:2}
-curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
+# first_two=${NADA_TOKEN:0:2}
+# curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
 
-json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
-
-curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
+# json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
+# https://data.intern.nav.no/story/87332165-41ff-49bc-bd34-a8747cefb3db
+# curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
 quarto render index.qmd
 
 

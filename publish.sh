de9076dd9572ba0ba676d49015ec1efaad13c9bb
oivindkulsrud <oivindkulsrud@gmail.com>
2024-02-07 09:56:48 +0100

    fungerer lokalt, bygger raskere

--- publish.sh
+++ publish.sh
@@ -7,12 +7,30 @@ set -e
 # json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
 # https://data.intern.nav.no/story/87332165-41ff-49bc-bd34-a8747cefb3db
 # curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
+#quarto render index.qmd
+#curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
+#echo "request started: " $curl_cmd
+#
+#response=$(curl -X PUT -F index.html=@index.html \
+#    "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
+#    -H "Authorization:Bearer ${NADA_TOKEN}" \
+#    --connect-timeout 30 \
+#    --silent --show-error --write-out "HTTP_STATUS:%{http_code}" \
+#    )
+#
+#
+#http_status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTP_STATUS://')
 
+date > index.html
 quarto render index.qmd
+response=$(curl -X PUT -F index.html=@index.html "https://${NADA_URL}/quarto/update/${QUARTO_ID}" -H "Authorization:Bearer ${NADA_TOKEN}")
 
+# Store the status code of the last executed command
+status=$?
 
-curl -X PUT -F index.html=@index.html \
-    "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
-    -H "Authorization:Bearer ${NADA_TOKEN}"
+echo "Response: $response"
+echo "HTTP Status: $http_status"
+
+# Check if the command
 
 

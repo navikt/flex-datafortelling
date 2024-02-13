85e9dce3f723ae8aef09ca73ce82591ab42cccfc
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-02-13 15:16:37 +0100

    testing env dependent running of files

--- publish.sh
+++ publish.sh
@@ -7,7 +7,21 @@ set -e
 # json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
 # https://data.intern.nav.no/story/87332165-41ff-49bc-bd34-a8747cefb3db
 # curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
-quarto render index.qmd
+
+if 
+
+# Assuming you have an environment variable named ENVIRONMENT
+if [ "$ENVIRONMENT" == "dev" ]; then
+    quarto render dev.qmd --output index.html
+elif [ "$ENVIRONMENT" == "prod" ]; then
+    quarto render prod.qmd --output index.html
+else
+    echo "assuming it is running locally"
+    quarto render dev.qmd --output dev.html
+    quarto render prod.qmd --output prod.html
+fi
+
+
 #curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
 #echo "request started: " $curl_cmd
 #

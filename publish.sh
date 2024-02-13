8237bf8126527c8065dbec7c56c683803e2acc2d
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-02-13 15:46:52 +0100

    wip

--- publish.sh
+++ publish.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# first_two=${NADA_TOKEN:0:2}
-# curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
-
-# json_payload=$(jq -n --arg cmd "$curl_cmd" '{"log": $cmd}')
-# https://data.intern.nav.no/story/87332165-41ff-49bc-bd34-a8747cefb3db
-# curl -X POST -d "$json_payload" -H 'Content-Type: application/json' http://localhost:19880/
-
 # Assuming you have an environment variable named ENVIRONMENT
 if [ "$ENVIRONMENT" == "dev" ]; then
     quarto render dev.qmd --output index.html
@@ -20,22 +13,6 @@ else
     quarto render prod.qmd --output index.html
 fi
 
-
-#curl_cmd="curl -X PUT -F index.html=@index.html \"https://${NADA_URL}/quarto/update/${QUARTO_ID}\" -H \"Authorization:Bearer not included for now\""
-#echo "request started: " $curl_cmd
-#
-#response=$(curl -X PUT -F index.html=@index.html \
-#    "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
-#    -H "Authorization:Bearer ${NADA_TOKEN}" \
-#    --connect-timeout 30 \
-#    --silent --show-error --write-out "HTTP_STATUS:%{http_code}" \
-#    )
-#
-#
-#http_status=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTP_STATUS://')
-
-# date > index.html
-# quarto render index.qmd
 response=$(curl -X PUT -F index.html=@index.html "https://${NADA_URL}/quarto/update/${QUARTO_ID}" -H "Authorization:Bearer ${NADA_TOKEN}")
 
 # Store the status code of the last executed command
@@ -44,6 +21,5 @@ status=$?
 echo "Response: $response"
 echo "HTTP Status: $http_status"
 
-# Check if the command
 
 

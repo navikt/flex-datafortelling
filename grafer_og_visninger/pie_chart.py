e748b6d7d08ecb26f23e1e1619cb368f182c44ed
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-04-22 13:57:42 +0200

    fullstendig data er skjult som default, viser kun relevant. Endret filstruktur

--- grafer_og_visninger/pie_chart.py
+++ grafer_og_visninger/pie_chart.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 def make_autopct(values):
     def my_autopct(pct):
         total = sum(values)
-        val = int(round(pct*total/100.0))
+        val = int(round(pct * total / 100.0))
         return f'{pct:.1f}%\n({val:d})'
+
     return my_autopct
+
 
 def enkel_pie_chart(katekorier, verdier, tittel, farger=None):
     # Find the index of the highest value in sizes
@@ -28,14 +30,14 @@ def enkel_pie_chart(katekorier, verdier, tittel, farger=None):
 
 
 # Function to plot side-by-side pie charts
-def side_ved_side_pie_chart(kategorier, verdi_foer, verdi_etter, tittel):
+def side_ved_side_pie_chart(kategorier, verdi_foer, verdi_etter):
     farger = ['#FF7F50', '#008080', '#9DC183', '#CDA4DE', '#87CEEB']  # Coral, Teal, Sage Green, Soft Violet, Sky Blue
 
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     axes[0].pie(verdi_foer, labels=kategorier, autopct=make_autopct(verdi_foer), startangle=75, colors=farger)
-    axes[0].set_title(f'{tittel} \nFØR ENDRING')
+    axes[0].set_title('FØR ENDRING')
     axes[0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     axes[1].pie(verdi_etter, labels=kategorier, autopct=make_autopct(verdi_etter), startangle=75, colors=farger)
-    axes[1].set_title(f'{tittel} \nETTER ENDRING')
+    axes[1].set_title('ETTER ENDRING')
     axes[1].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     plt.show()

3dabd0f8bf185b1d29a14f3d5e7d08fb308c8fba
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-03-20 09:08:04 +0100

    Bruk wget i stedet for curl

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ else
     quarto render prod.qmd --output index.html
 fi
 
-response=$(curl -X PUT -F index.html=@index.html "https://${NADA_URL}/quarto/update/${QUARTO_ID}" -H "Authorization:Bearer ${NADA_TOKEN}")
+response=$(wget --method=PUT --header="Authorization:Bearer ${NADA_TOKEN}" --body-file=index.html "https://${NADA_URL}/quarto/update/${QUARTO_ID}")
 
 # Store the status code of the last executed command
 status=$?
